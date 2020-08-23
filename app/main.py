8680dfdb3a7265fadd10cb50f3e3fada69ffb8f4
busgandy <39296111+busgandy@users.noreply.github.com>
2020-08-23 13:58:58 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def landing_page():
     return render_template ("index.html")
+
+@app.route("/static/images/title.jpg")
+def landing_page():
+    return render_template ("/static/images/title.jpg")
